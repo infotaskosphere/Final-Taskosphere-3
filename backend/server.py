81fbf7f982981b3840c351733e4beb1a65adc94f
infotaskosphere <info.taskosphere@gmail.com>
2026-02-14 21:25:38 +0530

    Update server.py

--- backend/server.py
+++ backend/server.py
@@ -1,3 +1,6 @@
+import smtplib
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
 from fastapi import FastAPI, APIRouter, Depends, HTTPException, status, BackgroundTasks
 from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
 from dotenv import load_dotenv
