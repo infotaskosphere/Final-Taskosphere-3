b43d5d04a3c2550e4c2dafe90023bacb6f2f8062
infotaskosphere <info.taskosphere@gmail.com>
2026-02-27 20:32:07 +0530

    Update dependencies.py

--- backend/dependencies.py
+++ backend/dependencies.py
@@ -1,11 +1,10 @@
+import os
 from motor.motor_asyncio import AsyncIOMotorClient
-from fastapi import Depends
 
-# Mongo setup
-MONGO_URL = "your_mongo_url_here"
+MONGO_URL = os.getenv("MONGO_URL")
+
+if not MONGO_URL:
+    raise Exception("MONGO_URL is not set")
 
 client = AsyncIOMotorClient(MONGO_URL)
 db = client["taskosphere"]
-
-# move get_current_user function here
-from backend.server import get_current_user
