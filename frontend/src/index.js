17a6beda307240a24752df86d9f8617ba1b51b3d
infotaskosphere <info.taskosphere@gmail.com>
2026-03-12 23:24:46 +0530

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,8 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
-import App from "./App";
-import { AuthProvider } from "./contexts/AuthContext";
+import "@/index.css";
+import App from "@/App";
+import { AuthProvider } from "@/contexts/AuthContext";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
