44731e85d48b4d48025a828173efc2f4415bdf0c
infotaskosphere <info.taskosphere@gmail.com>
2026-03-14 12:04:09 +0530

    Update __init__.py

--- backend/__init__.py
+++ backend/__init__.py
@@ -1,15 +1,1 @@
-from .cause_list import bp_cause
-from .application import bp_app
-from .agent import bp_agent
-from .search import bp_search
-from .efiling import bp_efiling
-from .portfolio import bp_portfolio
 
-__all__ = [
-"bp_cause",
-"bp_app",
-"bp_agent",
-"bp_search",
-"bp_efiling",
-"bp_portfolio"
-]
