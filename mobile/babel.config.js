9c35ff00c0ef4c930bc49c4ecdb11112411a1a59
infotaskosphere <info.taskosphere@gmail.com>
2026-03-17 22:35:03 +0530

    Update babel.config.js

--- mobile/babel.config.js
+++ mobile/babel.config.js
@@ -1,16 +1,13 @@
 module.exports = function (api) {
   api.cache(true);
+
   return {
     presets: [
-      // Standard Expo preset - we keep this simple for the production bundler
       ["babel-preset-expo", { jsxImportSource: "nativewind" }]
     ],
     plugins: [
-      // NativeWind v4 usually only needs the preset OR the plugin, not always both
       "nativewind/babel",
-      "react-native-worklets/plugin",
-      // Reanimated MUST be the absolute last item in the list
-      "react-native-reanimated/plugin",
+      "react-native-reanimated/plugin" // MUST always be last
     ],
   };
 };
