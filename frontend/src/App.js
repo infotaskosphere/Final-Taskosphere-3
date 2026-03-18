93f92234e52d1b6d1b387e4da3696b9fad4b61fe
infotaskosphere <info.taskosphere@gmail.com>
2026-03-18 11:53:53 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,9 @@
 import React, { Suspense } from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { AuthProvider } from "@/contexts/AuthContext";
 import { Toaster } from "@/components/ui/sonner";
 import AppRoutes from "./AppRoutes";
-import VisitsPage from "@/pages/VisitsPage";
 
 const queryClient = new QueryClient();
 
@@ -14,10 +13,7 @@ function App() {
       <AuthProvider>
         <BrowserRouter>
           <Suspense fallback={<div className="p-10">Loading...</div>}>
-            <Routes>
-              <Route path="/visits" element={<VisitsPage />} />
-              <Route path="/*" element={<AppRoutes />} />
-            </Routes>
+            <AppRoutes />
           </Suspense>
           <Toaster position="top-right" richColors />
         </BrowserRouter>
