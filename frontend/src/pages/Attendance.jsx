def4161f4769d2b2fa229dc1aadcdc95361e0d6c
infotaskosphere <info.taskosphere@gmail.com>
2026-04-05 00:24:21 +0530

    Update Attendance.jsx

--- frontend/src/pages/Attendance.jsx
+++ frontend/src/pages/Attendance.jsx
@@ -2657,8 +2657,9 @@ export default function Attendance() {
                   </div>
                 </SectionCard>
               )}
-            </div> 
-          
+            </div>
+          )}
+
           {/* ── RIGHT COLUMN: Recent Attendance + Location History ── */}
           <div className={isEveryoneView ? '' : 'xl:col-span-2 flex flex-col gap-4'}>
 
