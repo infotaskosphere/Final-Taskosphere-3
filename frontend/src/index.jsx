511cfc3534e234087dd811170bdf37ca4d22a594
infotaskosphere <info.taskosphere@gmail.com>
2026-08-22 15:32:37 +0530

    Update index.jsx

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
+import "./App.css";
 // ✅ FIXED: Explicit extensions for Vite
 import App from "./App.jsx";
 
